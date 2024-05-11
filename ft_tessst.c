41fa02856e77fa90dab52b27cf1b9adefccd1d76
Maxime Briand <mbriand@3B6.42angouleme.fr>
2024-05-11 18:32:55 +0200

    test

---
+++ ft_tessst.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void	ft_tessst(void)
+{
+	printf("Hello ");
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void	ft_tessst(void);
+
+int	main(void)
+{
+	ft_tessst();
+	printf("world\n");
+	return(0);
+}
